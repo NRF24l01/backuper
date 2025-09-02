1a275099fc442df116a9b6c98cae1abbd955bcb8
nrf24l01 <s.nevrovsky@ya.ru>
2025-09-02 23:21:59 +0300

    Add WorkerCapability model and update database migration

--- backend/models/dbRegister.go
+++ backend/models/dbRegister.go
@@ -20,7 +20,7 @@ func RegisterPostgres(cfg *core.Config) (*gorm.DB){
 
 	db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto";`)
 
-	if err := db.AutoMigrate(&User{}, &Worker{}); err != nil {
+	if err := db.AutoMigrate(&User{}, &Worker{}, &WorkerCapability{}); err != nil {
 		log.Fatalf("failed to migrate database: %v", err)
 	}
 
---
+++ backend/models/workerCapability.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"github.com/nrf24l01/go-web-utils/goorm"
+)
+
+type WorkerCapability struct {
+	goorm.BaseModel
+	WorkerID uuid.UUID     `gorm:"column:worker_id"`
+	Worker   *Worker       `gorm:"foreignKey:WorkerID"`
+	Type     string        `gorm:"type:varchar(100)"`
+	About    string        `gorm:"type:jsonb"`
+	ToBackup bool          `gorm:"default:false"`
+	BackupInterval uint64  `gorm:"default:0"`
+}
